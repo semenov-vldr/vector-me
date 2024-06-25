eb03ea16fffc01af0e99221d4219a90f43e3ddc7
Vladimir <vova2066@yandex.ru>
2024-06-25 23:02:46 +0300

    cont brief

--- src/blocks/brief/brief.js
+++ src/blocks/brief/brief.js
@@ -2,10 +2,26 @@ const brief = document.getElementById("brief");
 
 if (brief) {
 
-  const closeBrief = brief.querySelector(".brief__back");
+  const openBriefBtns = document.querySelectorAll(".js-brief-open");
 
-  closeBrief.addEventListener("click", () => {
+  console.log(openBriefBtns)
 
+  openBriefBtns.forEach(openBriefBtn => {
+    openBriefBtn.addEventListener("click", () => {
+      brief.classList.add("js-visible");
+      blockScrollBody();
+    })
+  })
+
+
+
+  const closeBriefBtn = brief.querySelector(".brief__back");
+
+  closeBriefBtn.addEventListener("click", () => {
+    brief.classList.remove("js-visible");
+    unblockScrollBody();
   });
+
+
 
 }
